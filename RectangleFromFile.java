0eb6e782ec9f211cb58a43718c004dde20ce5e19
Julian <jdominguez9@gmail.com>
2025-07-17 16:04:56 -0400

    Problem-Solving Activity 2: Debugging

--- RectangleFromFile.java
+++ RectangleFromFile.java
@@ -12,13 +12,19 @@ public class RectangleFromFile {
 
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
-
+            
+            try {
                 String[] parts = line.split(" ");
                 int width = Integer.parseInt(parts[0]);
                 int height = Integer.parseInt(parts[1]);
 
                 Rectangle r = new Rectangle(0, 0, width, height);
                 System.out.println("Rectangle: " + r + ", Area: " + (width * height));
+            } catch (NumberFormatException e) {
+                System.out.println("NumberFormatException: Skipping invalid line: \"" + line + "\"");
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("ArrayIndexOutOfBoundsException: Skipping invalid line: \"" + line + "\"");
+        }
             }
 
             scanner.close();
